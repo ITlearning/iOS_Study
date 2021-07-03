c5da13e2972d9bd411fd56ca6280207ea6670aef
ITlearning <yo7504@naver.com>
2021-07-03 20:14:10 +0900

    View Control

---
+++ ViewHierarchy/ViewHierarchy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ViewHierarchy
+//
+//  Created by IT learning on 2021/07/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
