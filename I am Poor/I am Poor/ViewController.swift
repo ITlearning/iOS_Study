393e1dd389f63e0dfaa87b15bc138fc18caf3c66
ITlearning <yo7504@naver.com>
2021-07-04 16:05:03 +0900

    Creating Funny Apps

---
+++ I am Poor/I am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Poor
+//
+//  Created by IT learning on 2021/07/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
